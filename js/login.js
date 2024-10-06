ee19adb9e66943a0a6ab0e6abacd084bd635ebe8
HammadMal <122615617+HammadMal@users.noreply.github.com>
2024-10-06 15:01:42 +0500

    login page css fixed

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    var errorMessage = document.getElementById('errorMessage');
+
     if (username === 'user_test' && password === 'test1234') {
-        window.location.href = 'homepage.html'; // Redirect to homepage.html
+        window.location.href = 'homepage.html';
     } else {
         console.log('Incorrect username or password');
-        document.getElementById('errorMessage').style.display = 'block';
+        errorMessage.style.opacity = '1'; // Make the error message visible
     }
 }
